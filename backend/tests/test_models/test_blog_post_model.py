971fc9425916647fc0900f58a8d955fcbda39e79
Egor Romanov <alchemy.follower666@gmail.com>
2025-01-08 04:15:32 +0300

    Small fixes and improvements for TestBlogPost, added test views python directory

--- backend/tests/test_models/test_blog_post_model.py
+++ backend/tests/test_models/test_blog_post_model.py
@@ -9,7 +9,9 @@ from backend.tests.config.config_make_string import make_random_string
 
 class TestBlogPost(TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
+        """Testing setup. Returns blog post object"""
+
         # Clear all cache at once for all cases
         ContentType.objects.clear_cache()
 
@@ -17,16 +19,18 @@ class TestBlogPost(TestCase):
         self.random_slug = make_random_string(30)
         self.random_content = make_random_string(4000)
 
-        # Creating user
-        self.user = BlogPost.objects.create(
+        # Creating Blog Post object
+        self.blog_post = BlogPost.objects.create(
             post_title=self.random_title,
             post_slug=self.random_slug,
             post_content=self.random_content
         )
 
-        return self.user
+        return self.blog_post
 
-    def tearDown(self) -> None:
+    def tearDown(self):
+        """Testing teardown. Deleting blog post object"""
+
         # Clear all cache at once for all cases
         ContentType.objects.clear_cache()
         print('Cache was cleared')
@@ -34,14 +38,40 @@ class TestBlogPost(TestCase):
         mb_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1000
         print(f'Memory usage: {mb_memory} MB')
 
-        del self.user
+        del self.blog_post
         print('All testing data was cleared')
 
     def test_post_title_field(self):
-        self.assertEqual(BlogPost.objects.all()[0].post_title, self.random_title)
+        """Test that post title is correct"""
+
+        post_title = BlogPost.objects.all()[0].post_title
+
+        self.assertEqual(post_title, self.random_title)
 
     def test_post_slug_field(self):
-        self.assertEqual(BlogPost.objects.all()[0].post_slug, self.random_slug)
+        """Test that post slug is correct"""
+
+        post_slug = BlogPost.objects.all()[0].post_slug
+
+        self.assertEqual(post_slug, self.random_slug)
 
     def test_post_content_field(self):
-        self.assertEqual(BlogPost.objects.all()[0].post_content, self.random_content)
+        """Test that post content is correct"""
+
+        post_content = BlogPost.objects.all()[0].post_content
+
+        self.assertEqual(post_content, self.random_content)
+
+    def test_post_str_method(self):
+        """Test that post str method returns correct value (post title)"""
+
+        post_str = BlogPost.objects.all()[0].__str__()
+
+        self.assertEqual(post_str, self.random_title)
+
+    def test_post_get_post_pk(self):
+        """Test that post get_post_pk method returns correct value (post pk)"""
+
+        post_pk = BlogPost.objects.all()[0].get_post_pk()
+
+        self.assertEqual(post_pk, self.blog_post.pk)
