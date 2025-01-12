9a675aa2c115df6003f3f26d86093ede736ea728
Egor Romanov <alchemy.follower666@gmail.com>
2025-01-12 08:02:28 +0300

    Added BlogPostAdmin to Admin Panel (to post new blogs);

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models import BlogPost
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"post_slug": ("post_title",)}
+    list_display = ('post_title', 'post_slug', 'created_date')
+    search_fields = ('post_title', 'post_content')
+    ordering = ('-created_date',)
+    date_hierarchy = 'created_date'
+
+admin.site.register(BlogPost, BlogPostAdmin)
