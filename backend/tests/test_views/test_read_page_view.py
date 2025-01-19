583630a84bd74331a424b81add821e2a847f7a40
Egor Romanov <lelouch.romanov@gmail.com>
2025-01-19 07:45:38 +0300

    Optimized unit tests code for ReadPostPage View (removed duplicates of code with json part);

--- backend/tests/test_views/test_read_page_view.py
+++ backend/tests/test_views/test_read_page_view.py
@@ -38,8 +38,9 @@ class TestPostReadView(TestCase):
                 }
             )
         )
+        self.json_data = json.loads(self.response.content)
 
-        return self.response
+        return self.json_data
 
     def tearDown(self):
         """Testing setup. Deleting blog post object"""
@@ -52,7 +53,7 @@ class TestPostReadView(TestCase):
         print(f'Memory usage: {mb_memory} MB')
 
         self.blog_post.delete()
-        del self.response
+        del self.json_data
         print('All testing data was cleared')
 
     def test_post_read_response_status(self):
@@ -63,8 +64,7 @@ class TestPostReadView(TestCase):
     def test_main_page_title_field_value(self):
         """Test that read post view returns correct title value"""
 
-        json_data = json.loads(self.response.content)
-        post_title = json_data['post_title']
+        post_title = self.json_data['post_title']
 
         self.assertEqual(post_title, self.random_title)
 
@@ -72,8 +72,7 @@ class TestPostReadView(TestCase):
         """Test that read post view returns
         correct title len smaller or equal 200"""
 
-        json_data = json.loads(self.response.content)
-        post_title = json_data['post_title']
+        post_title = self.json_data['post_title']
 
         self.assertTrue(len(post_title) <= 200)
 
@@ -81,8 +80,7 @@ class TestPostReadView(TestCase):
         """Test that read post view returns
         correct title len bigger than 0"""
 
-        json_data = json.loads(self.response.content)
-        post_title = json_data['post_title']
+        post_title = self.json_data['post_title']
 
         self.assertTrue(len(post_title) > 0)
 
@@ -90,8 +88,7 @@ class TestPostReadView(TestCase):
         """Test that read post view
         returns correct short content value"""
 
-        json_data = json.loads(self.response.content)
-        post_content_full = json_data['post_content']
+        post_content_full = self.json_data['post_content']
 
         self.assertEqual(post_content_full, self.random_content)
 
@@ -99,16 +96,14 @@ class TestPostReadView(TestCase):
         """Test that read post view returns
         correct short content smaller or equal 4000"""
 
-        json_data = json.loads(self.response.content)
-        post_content_full = json_data['post_content']
+        post_content_full = self.json_data['post_content']
 
         self.assertTrue(len(post_content_full) <= 4000)
 
     def test_post_read_date_field(self):
         """Test that posts collection view returns correct date"""
 
-        json_data = json.loads(self.response.content)
-        date_actual = json_data['post_date']
+        date_actual = self.json_data['post_date']
         date_expected = datetime.datetime.now().strftime('%Y-%m-%d')
 
         self.assertEqual(date_actual, date_expected)
@@ -116,7 +111,6 @@ class TestPostReadView(TestCase):
     def test_post_read_null_image_field(self):
         """Test that posts collection view returns correct null image"""
 
-        json_data = json.loads(self.response.content)
-        post_image = json_data['post_image']
+        post_image = self.json_data['post_image']
 
         self.assertEqual(post_image, '""')
