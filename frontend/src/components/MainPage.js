b2320c0aa8e9fa46f84e8ac8dd1e50b59f2cc730
Egor Romanov <lelouch.romanov@gmail.com>
2025-02-11 14:49:20 +0300

    Main logic of Django Secret Key generator is done; button refreshing keys and coping keys (to a clipboard);

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -46,13 +46,15 @@ const fetchData = async () => {
             <h1>Your Secret Key</h1>
             <h3 className="post-short">{data.key}</h3>
             <nav>
-                <a className="one" href="/owl-ai">Refresh</a>
-                <a className="zero" href="/contact">Copy</a>
+                <button className="one" onClick={() => fetchData()}>Refresh</button>
+                <button className="zero" onClick={() => {navigator.clipboard.writeText(data.key)}}>Copy</button>
             </nav>
         </section>
       </main>
 
       <footer className="footer">
+        <p><a href="https://docs.djangoproject.com/en/5.1/" target="_blank">Official Docs Django</a></p>
+        <p><a href="https://github.com/pen-alchemist/Eye-Diskage" target="_blank">Source Code</a></p>
         <p>&copy; 2025 by Yehor Romanov aka @pen-alchemist </p>
       </footer>
     </div>
