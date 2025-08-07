350ec121cf30171438a5bcd1a3f3c312ae5b9456
Egor Romanov <lelouch.romanov@gmail.com>
2025-08-08 02:09:06 +0300

    Updated Functional Tests (UI): Locators for About Page;

--- functional_tests/locators/about_page_locators.py
+++ functional_tests/locators/about_page_locators.py
@@ -1,14 +1,32 @@
 from selenium.webdriver.common.by import By
 
 
-class AboutPageLocators(object):
-    """A class for about page locators. All main page locators should come here"""
+class AboutPageLocators:
+    """
+    Centralized collection of locators for the About Page.
 
-    NAV_BUTTON_BLOG = (By.ID, 'nav-button-eye_diskage')
-    NAV_BUTTON_ABOUT = (By.ID, 'nav-button-about')
-    MAIN_HEADER = (By.ID, 'main-header2')
-    SUB_HEADER = (By.TAG_NAME, 'h1')
-    NO_POSTS_MESSAGE = (By.CLASS_NAME, 'no-posts')
-    PAGES_COUNTER = (By.ID, 'pages-counter')
-    RETURN_BUTTON = (By.XPATH, '//button[span[text()=\'Return to Blog\']]')
-    FOOTER_TEXT = (By.CLASS_NAME, 'footer')
+    Purpose:
+        - Store all locator definitions in one place for maintainability
+        - Avoid hardcoding selectors inside page objects or tests
+        - Provide clear, self-documenting constants for UI elements
+
+    Usage:
+        from functional_tests.locators.about_page_locators import AboutPageLocators
+        driver.find_element(*AboutPageLocators.NAV_BUTTON_ABOUT).click()
+    """
+
+    # Navigation Buttons
+    NAV_BUTTON_BLOG = (By.ID, "nav-button-eye_diskage")
+    NAV_BUTTON_ABOUT = (By.ID, "nav-button-about")
+
+    # Headers & Messages
+    MAIN_HEADER = (By.ID, "main-header2")
+    SUB_HEADER = (By.TAG_NAME, "h1")
+    NO_POSTS_MESSAGE = (By.CLASS_NAME, "no-posts")
+
+    # Pagination & Navigation
+    PAGES_COUNTER = (By.ID, "pages-counter")
+    RETURN_BUTTON = (By.XPATH, "//button[span[text()='Return to Blog']]")
+
+    # Footer
+    FOOTER_TEXT = (By.CLASS_NAME, "footer")
