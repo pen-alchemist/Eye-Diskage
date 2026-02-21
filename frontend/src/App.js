24c2cf1459051dc0d9a7f2e0aa954abf77b1872e
pen-alchemist <lelouch.romanov@gmail.com>
2026-02-21 22:57:58 +0300

    Frontend Refactorization: Updated App file;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,50 +5,20 @@ import Django from './components/DjangoPage';
 import RandomNumberPage from './components/RandomNumberPage'
 import CaesarCipherPage from './components/CaesarPage';
 import VigenereCipherPage from './components/VigenerePage';
-import MobileMain from './components/mobile/MobileMainPage'
-import MobileDjango from './components/mobile/MobileDjangoPage'
-import MobileRandomNumberPage from './components/mobile/MobileRandomNumberPage'
-import MobileCaesarCipherPage from './components/mobile/MobileCaesarPage'
-import MobileVigenereCipherPage from './components/mobile/MobileVigenerePage'
 import Auth from './components/Auth';
+
+import { ThemeProvider } from './ThemeContext';
+import GlobalLayout from './GlobalLayout';
 
 function App() {
   const [auth, authStatus] = useState({
-        isAuthenticated: false
+    isAuthenticated: false
   });
 
-  const [windowDimension, setWindowDimension] = useState(null);
-
-  useEffect(() => {
-    setWindowDimension(window.innerWidth);
-  }, []);
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimension(window.innerWidth);
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const isMobile = windowDimension <= 640;
-
   return (
-    <Router>
-        {isMobile ? (
-          <>
-          <Routes>
-            <Route path="/" element={<MobileMain authStatus={authStatus} />} />
-            <Route path="/main" element={<MobileMain authStatus={authStatus} />} />
-            <Route path="/django" element={<MobileDjango authStatus={authStatus} />} />
-            <Route path="/random/numbers" element={<MobileRandomNumberPage authStatus={authStatus} />} />
-            <Route path="/caesar" element={<MobileCaesarCipherPage authStatus={authStatus} />} />
-            <Route path="/vigenere" element={<MobileVigenereCipherPage authStatus={authStatus} />} />
-          </Routes>
-          </>
-        ) : (
-          <>
+    <ThemeProvider>
+      <Router>
+        <GlobalLayout>
           <Routes>
             <Route path="/" element={<Main authStatus={authStatus} />} />
             <Route path="/main" element={<Main authStatus={authStatus} />} />
@@ -57,9 +27,9 @@ function App() {
             <Route path="/caesar" element={<CaesarCipherPage authStatus={authStatus} />} />
             <Route path="/vigenere" element={<VigenereCipherPage authStatus={authStatus} />} />
           </Routes>
-          </>
-        )}
-    </Router>
+        </GlobalLayout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app and ThemeProvider', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/CYBERPUNK TIMELINE/i);
   expect(linkElement).toBeInTheDocument();
 });
